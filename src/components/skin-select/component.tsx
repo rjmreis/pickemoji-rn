b5f863393013d6e74d6917694855fe59eb69bc9c
Ricardo Reis <rjm.reis@gmail.com>
2019-12-30 13:25:10 +0100

    Horizontal scroll

--- src/components/skin-select/component.tsx
+++ src/components/skin-select/component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@ui-kitten/components';
 import { getEmoji } from '~/utils/emoji';
-import { Container, HintText, GroupButton } from './styles';
+import { Container, HintText, GroupButton, ScrollableContainer } from './styles';
 import { Props } from './';
 
 export const SkinSelectComponent = (props: Props) => {
@@ -12,14 +12,16 @@ export const SkinSelectComponent = (props: Props) => {
   return (
     <Container>
       <HintText>Set your default skin tone</HintText>
-      <GroupButton appearance="outline" status="basic">
-        <Button onPress={() => setSkinTone('none')}>{getEmoji(':raised_hand:', 'none')}</Button>
-        <Button onPress={() => setSkinTone('white')}>{getEmoji(':raised_hand:', 'white')}</Button>
-        <Button onPress={() => setSkinTone('creamWhite')}>{getEmoji(':raised_hand:', 'creamWhite')}</Button>
-        <Button onPress={() => setSkinTone('lightBrown')}>{getEmoji(':raised_hand:', 'lightBrown')}</Button>
-        <Button onPress={() => setSkinTone('brown')}>{getEmoji(':raised_hand:', 'brown')}</Button>
-        <Button onPress={() => setSkinTone('darkBrown')}>{getEmoji(':raised_hand:', 'darkBrown')}</Button>
-      </GroupButton>
+      <ScrollableContainer>
+        <GroupButton appearance="outline" status="basic">
+          <Button onPress={() => setSkinTone('none')}>{getEmoji(':raised_hand:', 'none')}</Button>
+          <Button onPress={() => setSkinTone('white')}>{getEmoji(':raised_hand:', 'white')}</Button>
+          <Button onPress={() => setSkinTone('creamWhite')}>{getEmoji(':raised_hand:', 'creamWhite')}</Button>
+          <Button onPress={() => setSkinTone('lightBrown')}>{getEmoji(':raised_hand:', 'lightBrown')}</Button>
+          <Button onPress={() => setSkinTone('brown')}>{getEmoji(':raised_hand:', 'brown')}</Button>
+          <Button onPress={() => setSkinTone('darkBrown')}>{getEmoji(':raised_hand:', 'darkBrown')}</Button>
+        </GroupButton>
+      </ScrollableContainer>
     </Container>
   );
 };
--- src/components/skin-select/styles.ts
+++ src/components/skin-select/styles.ts
@@ -2,12 +2,15 @@ import styled from 'styled-components/native';
 import { Text, ButtonGroup } from '@ui-kitten/components';
 
 export const Container = styled.View`
-  flex-direction: row;
-  flex-wrap: wrap;
+  flex-direction: column;
   justify-content: center;
   padding: 8px;
   background-color: rgb(240, 241, 244);
 `;
+
+export const ScrollableContainer = styled.ScrollView.attrs(() => ({
+  horizontal: true,
+}))``;
 
 export const HintText = styled(Text)`
   padding: 10px 0 5px;
